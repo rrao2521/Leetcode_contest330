84157a19b2f97487cc4843b4eaba8320fcba8c50
Rishabh rao <107990966+rrao2521@users.noreply.github.com>
2023-02-05 14:30:19 +0530

    Create count_collision_of_monkey.cpp

---
+++ count_collision_of_monkey.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int mpow(int x, int y){
+        int res =1;
+        while(y){
+            if(y&1)
+                res = ((long)res* x)% mod;
+            x = ((long)x * x)% mod;
+            y = y/2;
+        }
+        return res;
+    }
+    int monkeyMove(int n) {
+        return(mpow(2,n)-2 + mod) % mod;
+        
+    }
+};
